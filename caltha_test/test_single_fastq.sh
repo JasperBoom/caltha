6a575a0e1a5f7b2a4278e097cf5b305298f151a5
JasperBoom <jboom@infernum.nl>
2020-07-24 07:55:18 +0200

    Add more temp files.

---
+++ caltha_test/test_single_fastq.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+/home/jboom/Temp/caltha/src/caltha \
+    -i /home/jboom/Temp/caltha/tests/data/umi5_primer_single.fastq \
+    -t /home/jboom/Temp/test-output/tab.zip \
+    -z /home/jboom/Temp/test-output/zip.zip \
+    -b /home/jboom/Temp/test-output/blast.zip \
+    -f "fastq" \
+    -l "umi5" \
+    -a "primer" \
+    -u "20" \
+    -y "0.97" \
+    -c "1" \
+    -w "GGRKCHGGDACWGGDTGAAC" \
+    -r "GATCAWACAAATAAAGGTAWTCGATC" \
+    -d "/home/jboom/Temp/caltha_test/test-output" \
+    -@ "2"
